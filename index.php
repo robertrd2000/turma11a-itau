f2f99d8a57aa871764b479f8a4b2bbaea9a2fb79
robertrd2000 <74377826+robertrd2000@users.noreply.github.com>
2020-11-25 13:44:55 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,55 @@
-<h1>Bem vindo a aula de CI e CD do treinamento Gama Itau 11A</h1>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usuário</title>
+    <script type="text/javascript" src="login.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body onload="carregarusuario()">
 
-<h5>Mudando agora com o pessoal da turma</h5>
-<h4>
-  <? 
-  $i = 6;
-  $i = $i * 350;
+    <div class="Container">
+        <div class="row">
+            <div class="col-12">
+                <h3 style="text-align: center"> CENTRAL DO USUARIO </h3>
+            </div>
+        </div>
 
-  echo " O resultado da variável feita em PHP é: $i";
-  ?>
-</h4>
+        <div class="row">
+            <div id="foto" class="col-12">
+
+            </div>
+        </div>
+        <div class="row">
+            <div id="dados" class="col-12">
+                
+            </div>
+        </div>
+        <br><br>
+        <button onclick="window.location='musica.html'"type="button" class="btn btn-primary"Gravar Musica/button>
+        <br>    
+        <button onclick="window.location='relmusicas.html'"type="button" class="btn btn-primary"Pesquisar Musica/button>
+            <br> 
+
+    </div>
+    
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"></script>
+</body>
+</html>
