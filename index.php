c185072e6bf7057bf96cba8a8a05f80f08f4882e
robertrd2000 <74377826+robertrd2000@users.noreply.github.com>
2020-11-25 13:53:56 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,55 +1,40 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Usuário</title>
-    <script type="text/javascript" src="login.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
-<body onload="carregarusuario()">
+function carregarusuario(){
+    var usuariologado = localStorage.getItem("logado"); 
+    if (usuariologado==null) {
+        window.location="login.html";
+    }else {
+        var usuariojson= JSON.parse(usuariologado);
+        document.getElementById("foto").innerHTML = 
+        "<img width='25%' height='25%' alt='Foto não encontrada' src=imagens/"  + usuariologado.foto + ">";
+        document.getElementById("dados").innerHTML=
+        "<h3>" + usuariojson.nome + "<br>" +  usuariojson.email + "<br>ID:" + usuariojson.id + "<br></h3>";
+      }
+}
 
-    <div class="Container">
-        <div class="row">
-            <div class="col-12">
-                <h3 style="text-align: center"> CENTRAL DO USUARIO </h3>
-            </div>
-        </div>
-
-        <div class="row">
-            <div id="foto" class="col-12">
-
-            </div>
-        </div>
-        <div class="row">
-            <div id="dados" class="col-12">
-                
-            </div>
-        </div>
-        <br><br>
-        <button onclick="window.location='musica.html'"type="button" class="btn btn-primary"Gravar Musica/button>
-        <br>    
-        <button onclick="window.location='relmusicas.html'"type="button" class="btn btn-primary"Pesquisar Musica/button>
-            <br> 
-
-    </div>
+function logar() {
     
+    var objeto = {
+        "email": document.getElementById("txtemail").value,
+        "senha": document.getElementById("txtsenha").value
+    }
+    var cabecalho = {
+        method: "POST",
+        body: JSON.stringify(objeto),
+        headers: { "Content-type": "application/json" }
+    }
+
+    fetch("http://localhost:8080/login", cabecalho)
+        .then(res=> res.json())
+        .then(res=>{
+            localStorage.setItem("logado", json.stringify(res));
+            window.location="usuario.html";
+        })
+        .catch(err=>{
+            window.alert("Deu ruim");
+        });
 
 
 
 
 
-
-
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        crossorigin="anonymous"></script>
-</body>
-</html>
+}
